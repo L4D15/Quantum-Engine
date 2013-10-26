5ec03d49ae2d4e0a1d253434d6279295fd1de669
José Ladislao Lainez Ortega <thedragonladis@gmail.com>
2013-10-26 23:35:56 +0200

    Created CollisionBox class.

---
+++ include/quantum/CollisionBox.h
@@ -0,0 +1,16 @@
+#ifndef QUANTUM_COLLISIONBOX_H
+#define	QUANTUM_COLLISIONBOX_H
+
+class CollisionBox
+{
+public:
+    CollisionBox();
+    CollisionBox(const int x, const int y, const int w, const int h);
+    
+private:
+    
+};
+
+
+#endif	/* QUANTUM_COLLISIONBOX_H */
+
