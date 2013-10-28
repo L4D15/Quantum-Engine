b5958444b31c01d9d0ca63ec2b96480bb3bb5d96
José Ladislao Lainez Ortega <thedragonladis@gmail.com>
2013-10-28 19:35:49 +0100

    Added support for Lua scripting.

--- include/quantum/components/2D/Collisions.h
+++ include/quantum/components/2D/Collisions.h
@@ -3,6 +3,7 @@
 
 #include "quantum/CollisionBox.h"
 #include "quantum/components/Component.h"
+#include "quantum/GameObject.h"
 
 namespace components2D
 {
@@ -10,12 +11,17 @@ namespace components2D
 class Collisions : public components::Component
 {
 public:
-    Collisions();
+    Collisions(GameObject& owner);
     ~Collisions();
+
+    void setBoxGeometry(const int x, const int y, const int w, const int h);
 
 private:
     CollisionBox box;
 
+    // Trigger collisions
+    bool trigger;
+    std::string pathToScript;
 };
 
 }
--- src/engine/components/2D/Collisions.cpp
+++ src/engine/components/2D/Collisions.cpp
@@ -1,1 +1,9 @@
+#include "quantum/components/2D/Collisions.h"
 
+using namespace components2D;
+
+Collisions::Collisions(GameObject &owner):
+    Component(owner)
+{
+
+}
