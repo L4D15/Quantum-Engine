4b61c1823929b04d35f59f2ee974be312de6ee01
José Ladislao Lainez Ortega <thedragonladis@gmail.com>
2013-11-14 15:10:38 +0100

    Add tigger mechanism to Collisions component.

--- include/quantum/components/2D/Collisions.h
+++ include/quantum/components/2D/Collisions.h
@@ -4,23 +4,29 @@
 #include "quantum/components/Component.h"
 #include "quantum/Vector2D.h"
 
-namespace Components2D
+namespace components2D
 {
 
 class Collisions : public components::Component
 {
 public:
-    Collisions(GameObject& owner) : owner(owner) { }
+    Collisions(GameObject& owner) : Component(owner) { }
 
     inline float getOffsetX() { return offsetX; }
     inline float getOffsetY() { return offsetY; }
+    inline bool isTrigger() { return trigger; }
+    inline std::string getScriptToTrigger() { return script; }
 
     inline void setOffsetX(const float x) { offsetX = x; }
     inline void setOffsetY(const float y) { offsetY = y; }
+    inline void setIsTrigger(const bool t) { trigger = t; }
+    inline void setScriptToTrigger(const std::string s) { script = s; }
 
-private:
+protected:
     float offsetX;
     float offsetY;
+    bool trigger;
+    std::string script;
 };
 
 }
